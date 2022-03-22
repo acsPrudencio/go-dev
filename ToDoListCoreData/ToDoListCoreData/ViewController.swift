51e2a26fc4a393bc252c1c79d805167291e7289e
acsprudencio <acsprudencio@gmail.com>
2022-03-22 17:39:20 -0300

    Projeto ToDo ListCore Data - Criado

---
+++ ToDoListCoreData/ToDoListCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoListCoreData
+//
+//  Created by Idwall Go Dev 014 on 22/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
