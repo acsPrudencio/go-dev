25fb14db70ae477b860019ef2d90309a208403fb
acsprudencio <acsprudencio@gmail.com>
2022-03-27 16:48:29 -0300

    ProjetoFinal-Solo configuração inicial

---
+++ ProjetoFinal-Solo/ProjetoFinal/ProjetoFinal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjetoFinal
+//
+//  Created by Idwall Go Dev 014 on 27/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+    }
+
+
+}
+
